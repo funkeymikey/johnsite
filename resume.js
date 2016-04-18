91b8d00d1f138a48566828ffb4c04d414fadc5c3
Mike Kozelsky <mike.kozelsky@gmail.com>
2016-04-17 23:39:16 -0400

    broke everything

--- resume.js
+++ resume.js
@@ -1,13 +1,37 @@
-var resume = angular.module('resume', []);
+var resume = angular.module('resume', ['ngAnimate', 'ui.bootstrap', 'ngRoute']);
 
-resume.controller('resumeCtrl', function ($scope, $rootScope) {
+resume.config(function($routeProvider, $locationProvider) {
 
+  $routeProvider
+    .when('/', {
+      templateUrl: '/views/intro.html',
+      controller: 'resumeCtrl',
+      caseInsensitiveMatch: true
+    })
+    .when('/burchfield', {
+      templateUrl: '/views/burchfield.html',
+      controller: 'resumeCtrl',
+      caseInsensitiveMatch: true
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
 
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
+});
 
-  $scope.setView = function(view){
-    console.log('setting view to ' + view);
-    $scope.view = '/views/' + view;
+resume.controller('resumeCtrl', function ($scope, $uibModal, $location) {
+
+  $scope.setView = function(path){
+    $location.path(path);
   };
 
-    $scope.setView('intro.html');
+  $scope.open = function (template) {
+    var modalInstance = $uibModal.open({
+      animation: $scope.animationsEnabled,
+      size: 'lg',
+      templateUrl: template
+    });
+  };
 });
